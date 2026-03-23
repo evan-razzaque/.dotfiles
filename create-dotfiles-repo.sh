7766ab6b4d92cd0aa6815d591b5b8741cd14b3c0
evan-razzaque <evan.razzaque7@gmail.com>
2026-03-22 21:03:05 -0400

    Add create-dotfiles-repo.sh

---
+++ create-dotfiles-repo.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DOTFILES_PATH="$1"
+if [[ -z "$DOTFILES_PATH" ]]; then
+	echo "Missing argument" >&2
+	exit 1
+fi
+
+DOTFILES_PATH=$(readlink -f "$DOTFILES_PATH")
+cd $(dirname "$0")
+
+if [[ "$DOTFILES_PATH" == $(readlink -f "$PWD") ]]; then
+	echo "Cannot use existing dotfiles directory" >&2
+	exit 1
+fi
+
+mkdir "$DOTFILES_PATH" || exit $?
+mkdir "$DOTFILES_PATH/.ignore"
+
+cp -- *.sh README.md .gitignore "$DOTFILES_PATH"
+cp .ignore/README.txt "$DOTFILES_PATH/.ignore"
+
+cd "$DOTFILES_PATH"
+
+git init
+git add .
+git commit -m "Initial commit"
