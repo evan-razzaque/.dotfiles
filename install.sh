e5dbf96f237fac407a46cae9bda20ae35534f520
evan-razzaque <evan.razzaque7@gmail.com>
2026-04-20 13:47:55 -0400

    Support install/uninstall scripts for packages

--- install.sh
+++ install.sh
@@ -5,19 +5,10 @@ cd $(dirname $0)
 declare -a PACKAGES
 source stow-cmd.sh "$@"
 
-# Add git config option without duplicate values
-git-set-config() {
-	local option=$1
-	local value=$2
-	git config set --global --value $value --all $option $value
-}
-
-if [[ "${PACKAGES[*]}" =~ "git" ]]; then
-	git-set-config include.path .gitconfig-extra
-
-	if command -v gh > /dev/null; then
-		git-set-config include.path .gitconfig-credential
-	fi
-fi
-
 stow-cmd -R
+
+for package in "${PACKAGES[@]}"; do
+	SCRIPT="$package/install.sh"
+	[[ -f $SCRIPT ]] || continue
+	"./$SCRIPT"
+done
--- uninstall.sh
+++ uninstall.sh
@@ -5,16 +5,10 @@ cd $(dirname $0)
 declare -a PACKAGES
 source stow-cmd.sh "$@"
 
-# Remove git config option with a specific value
-git-unset-config() {
-	local option=$1
-	local value=$2
-	git config unset --global --value $value --all $option
-}
-
-if [[ "${PACKAGES[*]}" =~ "git" ]]; then
-	git-unset-config include.path .gitconfig-extra
-	git-unset-config include.path .gitconfig-credential
-fi
+for package in "${PACKAGES[@]}"; do
+	SCRIPT="$package/uninstall.sh"
+	[[ -f $SCRIPT ]] || continue
+	"./$SCRIPT"
+done
 
 stow-cmd -D
