5f57f29a66489817e092d2d5a61a9d39f263f71f
evan-razzaque <evan.razzaque7@gmail.com>
2026-07-09 11:41:10 -0400

    Update tmux-python-venv.sh

--- bash/.config/bash/bashrc.d/tmux-python-venv.sh
+++ bash/.config/bash/bashrc.d/tmux-python-venv.sh
@@ -3,12 +3,24 @@
 # Source (the command) warnings
 # shellcheck disable=SC1090,SC1091
 
-# Export and unset environment variables for tmux
-_tmux_export_unset() {
-	if [[ -z "${FUNCNAME[1]}" ]]; then
+# Prevent a function from being invoked outside of a function
+_tmux_guard_func() {
+	if [[ -z "${FUNCNAME[1]}" || -z "${FUNCNAME[2]}" ]]; then
 		echo "Cannot be called outside of a function" >&2
 		return 1
 	fi
+}
+
+_tmux_bash_func_export_name() {
+	_tmux_guard_func || return
+
+	local name=${1?}
+	echo "BASH_FUNC_$name%%"
+}
+
+# Export and unset environment variables for tmux
+_tmux_export_unset() {
+	_tmux_guard_func || return
 
 	export() {
 		builtin export "${@?}" || return
@@ -52,10 +64,10 @@ if [[ -n "$VIRTUAL_ENV" ]] && [[ -n "$_TMUX_SOURCE_VENV" ]]; then
 
 		_tmux_export_unset
 
-		eval "$($tmux_cmd show -v @venv-deactivate)"
+		# eval "$($tmux_cmd show -v @venv-deactivate)"
 		_deactivate
 
-		$tmux_cmd set -u @venv-deactivate
+		$tmux_cmd setenv -u "$(_tmux_bash_func_export_name _deactivate)"
 
 		unset _TMUX_SOURCE_VENV
 		unset -f export unset _deactivate deactivate
@@ -66,13 +78,18 @@ fi
 source-venv() {
 	local venv="${1:-.}"
 	local tmux_cmd="tmux"
-	[[ -z "$TMUX" ]] && tmux_cmd=:
+
+	if [[ -z "$TMUX" ]]; then
+		echo "${FUNCNAME[0]}: Must be in tmux session" >&2
+		return 1
+	fi
 
 	_tmux_export_unset
 
 	if source "$venv/bin/activate"; then
 		renamefunc deactivate _deactivate
-		$tmux_cmd set @venv-deactivate "$(declare -f _deactivate)"
+		$tmux_cmd setenv "$(_tmux_bash_func_export_name _deactivate)" "() {  \
+			$(declare -f _deactivate | tail -n +3)"
 
 		_TMUX_SOURCE_VENV=1
 		source "${BASH_SOURCE[0]}"
@@ -83,7 +100,5 @@ source-venv() {
 		export _TMUX_SOURCE_VENV
 	fi
 
-	# Preserve _deactivate while outside of tmux
-	[[ -n "$TMUX" ]] && unset -f _deactivate
 	unset -f export unset
 }
